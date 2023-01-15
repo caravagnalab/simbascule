c8f72900888f20849c148b9d93ed21e44710709c
azadsadr <azad.sadr.h@gmail.com>
2023-01-15 21:59:10 +0100

    change the basilica to simbasilica in eval_data

--- R/eval_data.R
+++ R/eval_data.R
@@ -1,5 +1,4 @@
 #----------------------------------------------------------------------QC:PASSED
-
 #' @import dplyr
 evaluate.data <- function(x) {
   #--------------------------
@@ -8,7 +7,7 @@ evaluate.data <- function(x) {
   expected_fixed <- x$exp_fixed[[1]]
   #inferred_fixed <- x$inf_fixed[[1]]
   inferred_fixed <- x$fit[[1]]$catalogue_signatures
-  a <- basilica:::fixed.accuracy(reference, input, expected_fixed, inferred_fixed)
+  a <- simbasilica:::fixed.accuracy(reference, input, expected_fixed, inferred_fixed)
   TP <- a$TP
   FP <- a$FP
   TN <- a$TN
@@ -19,24 +18,24 @@ evaluate.data <- function(x) {
   #alpha <- x$inf_exposure[[1]]
   alpha <- x$fit[[1]]$exposure
   beta <- rbind(x$fit[[1]]$catalogue_signatures, x$fit[[1]]$denovo_signatures)
-  mr <- basilica:::reconstruct.count(m, alpha, beta)
-  mae <- basilica:::compute.mae(m, mr)
-  mse <- basilica:::compute.mse(m, mr)
+  mr <- simbasilica:::reconstruct.count(m, alpha, beta)
+  mae <- simbasilica:::compute.mae(m, mr)
+  mse <- simbasilica:::compute.mse(m, mr)
   #--------------------------
-  b <- basilica:::denovo.similarity(x$exp_denovo[[1]], x$fit[[1]]$denovo_signatures)
+  b <- simbasilica:::denovo.similarity(x$exp_denovo[[1]], x$fit[[1]]$denovo_signatures)
   denovo_similarity <- b$similarity_average  # numeric
   denovo_match <- b$match_df                 # data.frame
   #--------------------------
-  denovo_ratio <- basilica:::denovo.ratio(x$exp_denovo[[1]], x$fit[[1]]$denovo_signatures)
+  denovo_ratio <- simbasilica:::denovo.ratio(x$exp_denovo[[1]], x$fit[[1]]$denovo_signatures)
   #--------------------------
-  
+
   # CREATE TIBBLE ----------------------------
   obj <- tibble::tibble(
-    
+
     targetX = x$targetX,
     inputX = x$inputX,
     num_samples = nrow(m),
-    
+
     mae = mae,
     mse = mse,
     fixed_acc = accuracy,
@@ -44,6 +43,6 @@ evaluate.data <- function(x) {
     denovo_sim = denovo_similarity,
     denovo_match = list(denovo_match),
   )
-  
+
   return(obj)
 }
