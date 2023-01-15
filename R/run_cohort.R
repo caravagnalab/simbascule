11ea651a09f76962e3493778f09997a40513c87b
azadsadr <azad.sadr.h@gmail.com>
2023-01-15 22:00:23 +0100

    eliminate some unused args

--- R/run_cohort.R
+++ R/run_cohort.R
@@ -6,27 +6,23 @@ run.cohort <- function(
     steps,
     phi,
     delta,
-    lambda_rate = NULL,
-    sigma = FALSE,
     input=TRUE
 ) {
-  
+
   results <- NULL
   for (i in 1:nrow(cohort)) {
-    
+
     cat('============================================\n') # TEST
     cat('                 Data No.', i, '\n') # TEST
     cat('============================================\n') # TEST
-    
-    xx <- basilica:::run.data(
+
+    xx <- simbasilica:::run.data(
       data = cohort[i, ],
       k = k,
       lr = lr,
       steps = steps,
       phi = phi,
       delta = delta,
-      lambda_rate = lambda_rate,
-      sigma = sigma,
       input = input
     )
     results <- rbind(results, xx)
