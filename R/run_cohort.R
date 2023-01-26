174da53f9df99abd745d5a4327da05c6c74f13a8
azadsadr <azad.sadr.h@gmail.com>
2023-01-26 17:11:55 +0100

    updated run_data and run_cohort function args

--- R/run_cohort.R
+++ R/run_cohort.R
@@ -1,29 +1,55 @@
 #----------------------------------------------------------------------QC:PASSED
 run.cohort <- function(
-    cohort,
+    x,
     k,
-    lr,
-    steps,
-    phi,
-    delta,
-    input=TRUE
+    #reference_catalogue = basilica::COSMIC_catalogue,
+    #input_catalogue = basilica::COSMIC_catalogue["SBS1", ],
+    cohort = "MyCohort",
+    use_reference = TRUE,
+    input = FALSE,
+    lr = 0.01,
+    steps = 500,
+    max_iterations = 20,
+    blacklist = NULL,
+    phi = 0.05,
+    delta = 0.9,
+    filt_pi =0.1,
+    groups = NULL,
+    lambda_rate = NULL,
+    sigma = FALSE,
+    CUDA = FALSE,
+    compile = TRUE,
+    enforce_sparsity = FALSE
 ) {
 
   results <- NULL
-  for (i in 1:nrow(cohort)) {
+  for (i in 1:nrow(x)) {
 
     cat('============================================\n') # TEST
     cat('                 Data No.', i, '\n') # TEST
     cat('============================================\n') # TEST
 
     xx <- simbasilica:::run.data(
-      data = cohort[i, ],
-      k = k,
+      data=x[i, ],
+      k=k,
+      #reference_catalogue = basilica::COSMIC_catalogue,
+      #input_catalogue = basilica::COSMIC_catalogue["SBS1", ],
+      cohort = paste0(cohort, "-", i),
+      use_reference = use_reference,
+      input = input,
       lr = lr,
       steps = steps,
+      max_iterations = max_iterations,
+      blacklist = blacklist,
       phi = phi,
       delta = delta,
-      input = input
+      filt_pi = filt_pi,
+      groups = groups,
+      lambda_rate = lambda_rate,
+      sigma = sigma,
+      CUDA = CUDA,
+      compile = compile,
+      enforce_sparsity = enforce_sparsity
     )
     results <- rbind(results, xx)
   }
