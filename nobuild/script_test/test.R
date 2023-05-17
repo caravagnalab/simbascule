8ac4d737300fbd377d5c30acc241f40c50a6b36c
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-05-17 14:31:26 +0200

    adding examples

--- nobuild/script_test/test.R
+++ nobuild/script_test/test.R
@@ -5,73 +5,103 @@ source("~/GitHub/simbasilica/nobuild/script_test/helper_fns.R")
 py = reticulate::import_from_path("pybasilica","~/GitHub/pybasilica/")
 devtools::load_all("~/GitHub/basilica/")
 devtools::load_all()
+
+path = "~/GitHub/simbasilica/nobuild/script_test/simulations/"
 # source("./R/generate_data.R")
 # source("./R/signatures.R")
 # source("./R/input.R")
 # source("./R/exposure.R")
 # source("./R/theta.R")
 
+make_plots = function(x, x.true=NULL, reconstructed=T, cls = NULL) {
+  mm = plot_mutations(x, reconstructed = reconstructed)
+  alp = plot_exposures(x, sort_by="SBS1", cls=cls)
+  bet = plot_signatures(x, cls=cls)
+
+  if (is.null(x.true)) return(patchwork::wrap_plots(bet + (mm/alp), guides="collect"))
+
+  mm.true = plot_mutations(x.true, reconstructed = F)
+  alpha.true = plot_exposures(x.true, sort_by="SBS1", cls=cls)
+
+  return(patchwork::wrap_plots(bet + (mm/mm.true/alp/alpha.true), guides="collect"))
+}
+
 # Generate inputs ####
 
 # cosmic = read.csv("./script_test/COSMIC_v3.3.1_SBS_GRCh38.txt", sep="\t") %>%
 #   tibble::column_to_rownames(var="Type") %>% t()
 
-cosmic = COSMIC_filt_merged
 cosine_limit = .8
 n_fixed = 2 # n of fixed signatures
-
-## De Novo signatures ####
 
 shared = c("SBS1", "SBS40 SBS3 SBS5")
 private_common = c("SBS17b", "SBS2", "SBS20")
 private_rare = c("SBS8 SBS4")
 
-denovo_catalogue = cosmic[c(private_common, private_rare),]
-
+denovo_catalogue = COSMIC_filt_merged[c(private_common, private_rare),]
 # reference catalogue is all cosmic - the sigs selected as private common/rare and shared
 # reference_cat = cosmic[!rownames(cosmic) %in% keep,]
 # reference_cat = select_fixed_sbs(reference_cat, n_fixed=n_fixed, cosine_limit=cosine_limit)
-reference_catalogue = cosmic[shared,]
-
-
-## Cosine similarity ####
+reference_catalogue = COSMIC_filt_merged[shared,]
 denovo_cosine = lsa::cosine(denovo_catalogue %>% t())
 reference_cosine = lsa::cosine(reference_catalogue %>% t())
 
 
-## Generate dataset ####
+# First datasets - rare ####
 
 # x = single_dataset(350, 2, 150:350, reference_catalogue, denovo_catalogue, reference_cosine, denovo_cosine,
 #                    private_sigs=list("rare"=private_rare,"common"=private_common),
-#                    private_fracs=list("rare"=0.05,"common"=0.3),
+#                    private_fracs=list("rare"=0.05,"common"=0.3), cohort_name="rare",
 #                    cosine_limit, seed=23, out_path="./nobuild/script_test/simulations/")
 
-x.simul = readRDS("./nobuild/script_test/simulations/simul.N350.G2.s23.Rds")
+x.simul = readRDS(paste0(path, "simul.N350.G2.s23.rare.Rds"))
 xx = create_basilica_obj_simul(x.simul)
 make_plots(xx, reconstructed = F)
 
-
 ## Old model fit, no groups
-# x.fit = fit(x.simul$x[[1]], k=0:7, py=py,
-#             input_catalogue=COSMIC_filt_merged["SBS1",],
-#             reference_catalogue=COSMIC_filt_merged,
-#             reg_bic=TRUE,
-#             filtered_cat=TRUE,
-#             regularizer = "KL")
-# plot_mutations(x.fit)
-# plot_exposures(x.fit, sort_by="SBS1")
-# plot_signatures(x.fit)
+x.fit.good = x.fit
+x.fit = fit(x.simul$x[[1]], k=0:7, py=py,
+            input_catalogue=COSMIC_filt_merged[c("SBS1","SBS40 SBS3 SBS5"),],
+            reference_catalogue=COSMIC_filt_merged,
+            reg_bic=TRUE,
+            filtered_cat=TRUE,
+            regularizer = "cosine")
 
 
-## Old model fit, with groups
-# x.fit.g = fit(x$x[[1]], k=1:7, py=py, input_catalogue=NULL,
-#             reference_catalogue=COSMIC_filt_merged,
-#             reg_bic=TRUE, filtered_cat=TRUE, groups=x$groups[[1]]-1,
-#             regularizer="KL")
-# x.fit.g$groups = x$groups[[1]]-1
-# plot_mutations(x.fit.g)
-# plot_exposures(x.fit.g, sort_by="SBS1")
-# plot_signatures(x.fit.g)
+# Old model fit, with groups
+x.fit.g = fit(x.simul$x[[1]], k=0:7, py=py,
+              input_catalogue=COSMIC_filt_merged[c("SBS1","SBS40 SBS3 SBS5"),],
+              reference_catalogue=COSMIC_filt_merged,
+              reg_bic=TRUE, filtered_cat=TRUE, groups=x.simul$groups[[1]]-1,
+              regularizer = "cosine")
+x.fit.g$groups = x.simul$groups[[1]]-1
+
+
+sigs = c(rownames(get_signatures(xx)),
+         rownames(get_signatures(x.fit)),
+         rownames(get_signatures(x.fit.g))) %>% unique()
+cls = gen_palette(length(sigs)) %>% setNames(sigs)
+
+
+
+true.plots = make_plots(xx, reconstructed=F, cls=cls) & theme(legend.position="bottom")
+x1.plots = make_plots(x.fit, xx, reconstructed=T, cls=cls) & theme(legend.position="bottom")
+x2.plots = make_plots(x.fit.g, xx, reconstructed=T, cls=cls) & theme(legend.position="bottom")
+
+
+pdf("~/GitHub/hBasilica/results/fit_simulated/report.simul_tests.norare.pdf", height = 8, width = 14)
+true.plots & patchwork::plot_annotation(title="True data")
+x1.plots & patchwork::plot_annotation(title="Fit 1")
+x2.plots & patchwork::plot_annotation(title="Fit 2 - hierarchical")
+# patchwork::wrap_plots(plot_signatures(x1, signatures="SBS20", cls=cls) /
+#                         plot_signatures(x2,
+#                                         signatures=setdiff(rownames(get_signatures(x2)),
+#                                                            rownames(get_signatures(x1))),
+#                                         cls=cls))
+dev.off()
+
+
+
 
 
 
@@ -83,48 +113,34 @@ x.fit.new = two_steps_inference(x.simul$x[[1]],
                                 enforce_sparsity2=FALSE,
                                 py=py)
 x.new = x.fit.new$tot
-make_plots(x.new, reconstructed = T)
+make_plots(x.new, xx, reconstructed = T)
 
-saveRDS(x.new, "~/GitHub/simbasilica/nobuild/test_new1.Rds")
+# saveRDS(x.new, "~/GitHub/simbasilica/nobuild/test_new1.Rds")
+#
+#
+#
+# x.fit.new2 = two_steps_inference(x.simul$x[[1]],
+#                                 k=0:7,
+#                                 input_catalogue=COSMIC_filt_merged[rownames(COSMIC_filt_merged)!="SBS20",],
+#                                 enforce_sparsity1=TRUE,
+#                                 enforce_sparsity2=FALSE,
+#                                 py=py)
+# x.new2 = x.fit.new2$tot
+# make_plots(x.new2, xx, reconstructed = T)
+#
+# patchwork::wrap_plots(plot_signatures(x.new2, signatures = c("SBS14","SBS44")) /
+#                         plot_signatures(x.new, signatures = "SBS20"))
 
 
 
-x.fit.new2 = two_steps_inference(x.simul$x[[1]],
-                                k=0:7,
-                                input_catalogue=COSMIC_filt_merged[rownames(COSMIC_filt_merged)!="SBS20",],
-                                enforce_sparsity1=TRUE,
-                                enforce_sparsity2=FALSE,
-                                py=py)
-x.new2 = x.fit.new2$tot
-make_plots(x.new2, xx, reconstructed = T)
-
-patchwork::wrap_plots(plot_signatures(x.new2, signatures = c("SBS14","SBS44")) /
-                        plot_signatures(x.new, signatures = "SBS20"))
-
-# pdf("./heatmap.pdf", height = 20, width = 10)
-# print(plot_similarity_reference(x.new2, reference=COSMIC_filt_merged))
-# dev.off()
-
-make_plots = function(x, x.true=NULL, reconstructed=T, cls = NULL) {
-  mm = plot_mutations(x, reconstructed = reconstructed)
-  alp = plot_exposures(x, sort_by="SBS1", cls=cls)
-  bet = plot_signatures(x, cls=cls)
-
-  if (is.null(x.true)) return(patchwork::wrap_plots(bet.new + (mm/alp), guides="collect"))
-
-  mm.true = plot_mutations(x.true, reconstructed = F, cls=cls)
-  alpha.true = plot_exposures(x.true, sort_by="SBS1", cls=cls)
-
-  return(patchwork::wrap_plots(bet.new + (mm/mm.true/alp/alpha.true), guides="collect"))
-}
-
+# Second datasets - no rare #####
 
 cosine_limit = .8
 n_fixed = 2
 
 shared = c("SBS1", "SBS40 SBS3 SBS5")
-private_common = c("SBS8 SBS4", "SBS2", "SBS20")
-private_rare = c("SBS17b")
+private_common = c("SBS8 SBS4", "SBS2", "SBS20", "SBS17b")
+private_rare = c()
 
 reference_catalogue = COSMIC_filt_merged[shared,]
 denovo_catalogue = COSMIC_filt_merged[c(private_common, private_rare),]
@@ -132,20 +148,67 @@ denovo_catalogue = COSMIC_filt_merged[c(private_common, private_rare),]
 denovo_cosine = lsa::cosine(denovo_catalogue %>% t())
 reference_cosine = lsa::cosine(reference_catalogue %>% t())
 
-x.simul2 = single_dataset(500, 2, 150:350, reference_catalogue, denovo_catalogue,
-                         reference_cosine, denovo_cosine,
-                         private_sigs=list("rare"=private_rare,"common"=private_common),
-                         private_fracs=list("rare"=0.05,"common"=0.3),
-                         cosine_limit, seed=23, out_path="./nobuild/script_test/simulations/")
+# x.simul2 = single_dataset(350, 2, 150:350, reference_catalogue, denovo_catalogue,
+#                          reference_cosine, denovo_cosine,
+#                          private_sigs=list("rare"=private_rare,"common"=private_common),
+#                          private_fracs=list("rare"=0.05,"common"=0.3), cohort_name="norare",
+#                          cosine_limit, seed=23, out_path="./nobuild/script_test/simulations/")
+x.simul2 = readRDS("~/GitHub/simbasilica/nobuild/script_test/simulations/simul.N350.G2.s23.norare.Rds")
 xx2 = create_basilica_obj_simul(x.simul2)
-x.fit2 = two_steps_inference(x.simul2$x[[1]],
-                             k=0:7,
-                             input_catalogue=COSMIC_filt_merged,
-                             enforce_sparsity1=TRUE,
-                             enforce_sparsity2=FALSE,
-                             py=py)
-x.new2 = x.fit2$tot
-saveRDS(x.new2, "~/GitHub/simbasilica/nobuild/test_new1.Rds")
+make_plots(xx2, reconstructed = F)
+
+## Old model fit, no groups
+x.fit = fit(x.simul2$x[[1]], k=0:7, py=py,
+            input_catalogue=COSMIC_filt_merged[c("SBS1","SBS40 SBS3 SBS5"),],
+            reference_catalogue=COSMIC_filt_merged,
+            reg_bic=TRUE,
+            filtered_cat=TRUE,
+            regularizer = "cosine")
+
+
+# Old model fit, with groups
+x.fit.g = fit(x.simul2$x[[1]], k=0:7, py=py,
+              input_catalogue=COSMIC_filt_merged[c("SBS1","SBS40 SBS3 SBS5"),],
+              reference_catalogue=COSMIC_filt_merged,
+              reg_bic=TRUE, filtered_cat=TRUE, groups=x.simul2$groups[[1]]-1,
+              regularizer = "cosine")
+x.fit.g$groups = x.simul$groups[[1]]-1
+
+
+sigs = c(rownames(get_signatures(xx)),
+         rownames(get_signatures(x.fit)),
+         rownames(get_signatures(x.fit.g))) %>% unique()
+cls = gen_palette(length(sigs)) %>% setNames(sigs)
+
+
+
+true.plots = make_plots(xx, reconstructed=F, cls=cls) & theme(legend.position="bottom")
+x1.plots = make_plots(x.fit, xx, reconstructed=T, cls=cls) & theme(legend.position="bottom")
+x2.plots = make_plots(x.fit.g, xx, reconstructed=T, cls=cls) & theme(legend.position="bottom")
+
+
+pdf("~/GitHub/hBasilica/results/fit_simulated/report.simul_tests.rare.pdf", height = 8, width = 14)
+true.plots & patchwork::plot_annotation(title="True data")
+x1.plots & patchwork::plot_annotation(title="Fit 1")
+x2.plots & patchwork::plot_annotation(title="Fit 2 - hierarchical")
+# patchwork::wrap_plots(plot_signatures(x1, signatures="SBS20", cls=cls) /
+#                         plot_signatures(x2,
+#                                         signatures=setdiff(rownames(get_signatures(x2)),
+#                                                            rownames(get_signatures(x1))),
+#                                         cls=cls))
+dev.off()
+
+
+
+
+# x.fit2 = two_steps_inference(x.simul2$x[[1]],
+#                              k=0:7,
+#                              input_catalogue=COSMIC_filt_merged,
+#                              enforce_sparsity1=TRUE,
+#                              enforce_sparsity2=FALSE,
+#                              py=py)
+# x.new2 = x.fit2$tot
+# saveRDS(x.new2, "~/GitHub/simbasilica/nobuild/test_new1.Rds")
 
 
 
