f81cbcf0dca074b86b910f54cdbc0043685b61d5
elena-buscaroli <ele.buscaroli@gmail.com>
2023-06-01 14:20:53 +0200

    adding fits on old model

---
+++ nobuild/simulations/fits_old_model_3005/run_oldmodel_3005.R
@@ -0,0 +1,34 @@
+main_path = "/u/cdslab/ebusca00/scratch_shared/basilica_pkgs/"
+out_path = paste0(main_path, "simbasilica/nobuild/simulations/simulations_2905/")
+
+reticulate::use_condaenv("basilica-env")
+py = reticulate::import_from_path(module = "pybasilica", path = paste0(main_path,"pybasilica/"))
+
+devtools::load_all(paste0(main_path, "basilica"))
+devtools::load_all(paste0(main_path, "simbasilica"))
+
+source(paste0(main_path, "simbasilica/nobuild/script_test/helper_fns.R"))
+library(ggplot2)
+
+comb = tibble(
+  N_vals = c(50, 300, 300, 1000, 1000, 5000, 5000),
+  n_groups_vals = c(2, 2, 4, 4, 6, 6, 10),
+  samples_per_group = list(25:50, 50:300, 50:300, 100:1000, 100:1000, 200:5000, 200:5000),
+  n_priv_comm = c(2, 2, 4, 4, 5, 5, 5),
+  n_priv_rare = c(1, 1, 1, 1, 2, 2, 3)
+)
+
+shared = c("SBS1", "SBS5", "SBS17b")
+private = c("SBS10b", "SBS28", "SBS56 SBS10a", "SBS90", "SBS2", "SBS13", "SBS20", "SBS22")
+
+generate_synthetic_datasets(shared = shared,
+                            private = private,
+                            catalogue = COSMIC_filt_merged,
+                            comb_matrix = comb,
+                            py = py,
+                            CUDA = TRUE,
+                            reg_weight = 0,
+                            out_path = out_path,
+                            seeds = 1:20,
+                            do.fits = TRUE,
+                            verbose = FALSE)
