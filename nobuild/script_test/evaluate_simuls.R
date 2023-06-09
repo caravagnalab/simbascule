3b19f48b599c2ab1a0657dc7f0f17d2fda17e63d
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-06-09 11:56:08 +0200

    minor changes

--- nobuild/script_test/evaluate_simuls.R
+++ nobuild/script_test/evaluate_simuls.R
@@ -2,69 +2,100 @@ devtools::load_all("~/GitHub/basilica/")
 devtools::load_all("~/GitHub/simbasilica/")
 library(ggplot2)
 
-fits_path = "~/GitHub/simbasilica/nobuild/simulations/fits_new_model_3105/"
+fits_path = "~/GitHub/simbasilica/nobuild/simulations/run_new_model_0806/"
+# fits_path = "~/GitHub/simbasilica/nobuild/simulations/run_oldmodel_regul_0806/"
 data_path = "~/GitHub/simbasilica/nobuild/simulations/synthetic_datasets/"
 
 fits = list.files(path=fits_path,
                    pattern="fit.")
 
-stats = lapply(fits, function(fitname)
-  compare_single_fit(fitname, fits_path, data_path)
-  ) %>% do.call(what=rbind, args=.)
+stats = lapply(fits, function(fitname) {
+  print(fitname)
+  compare_single_fit(fitname, fits_path, data_path, cutoff=0.8)
+  } ) %>% do.call(what=rbind, args=.) %>%
+  dplyr::mutate(inf_type=ifelse(is_hierarchical, "Hierarchical", "Non-hierarchical"))
 
 
-stats %>%
+mse_counts = stats %>%
   ggplot() +
-  geom_jitter(aes(x=is_hierarchical, y=mse_counts), size=.1) +
-  geom_boxplot(aes(x=is_hierarchical, y=mse_counts), alpha=0) +
-  facet_grid(N~G) +
+  geom_jitter(aes(x=as.factor(N), y=mse_counts, color=inf_type), size=.1) +
+  geom_boxplot(aes(x=as.factor(N), y=mse_counts, color=inf_type), alpha=0) +
+  facet_grid(~G, scales="free_x") +
   theme_bw() + labs(title="MSE between true and reconstructed counts")
 
 
-stats %>%
+mse_expos = stats %>%
   ggplot() +
-  geom_jitter(aes(x=is_hierarchical, y=mse_expos), size=.1) +
-  geom_boxplot(aes(x=is_hierarchical, y=mse_expos), alpha=0) +
-  facet_grid(N~G) +
+  geom_jitter(aes(x=as.factor(N), y=mse_expos, color=inf_type), size=.1) +
+  geom_boxplot(aes(x=as.factor(N), y=mse_expos, color=inf_type), alpha=0) +
+  facet_grid(~G, scales="free_x") +
   theme_bw() + labs(title="MSE between true and estimated exposures")
 
 
-stats %>%
+cos_sigs = stats %>%
   ggplot() +
-  geom_jitter(aes(x=is_hierarchical, y=cosine_sigs), size=.1) +
-  geom_boxplot(aes(x=is_hierarchical, y=cosine_sigs), alpha=0) +
-  facet_grid(N~G) +
-  theme_bw() + labs(title="Cosine between true and estimated signatures")
-
-stats %>%
-  ggplot() +
-  geom_jitter(aes(x=factor(G), y=cosine_sigs), size=.1) +
-  geom_boxplot(aes(x=factor(G), y=cosine_sigs), alpha=0) +
-  facet_grid(N~is_hierarchical) +
+  geom_jitter(aes(x=factor(N), y=cosine_sigs, color=inf_type), size=.1) +
+  geom_boxplot(aes(x=factor(N), y=cosine_sigs, color=inf_type), alpha=0) +
+  facet_grid(~G, scales="free_x") +
   theme_bw() + labs(title="Cosine between true and estimated signatures")
 
 
-stats %>%
+cos_expos = stats %>%
   ggplot() +
-  geom_jitter(aes(x=is_hierarchical, y=cosine_expos), size=.1) +
-  geom_boxplot(aes(x=is_hierarchical, y=cosine_expos), alpha=0) +
-  facet_grid(N~G) +
+  geom_jitter(aes(x=factor(N), y=cosine_expos, color=inf_type), size=.1) +
+  geom_boxplot(aes(x=factor(N), y=cosine_expos, color=inf_type), alpha=0) +
+  facet_grid(~G, scales="free_x") +
   theme_bw() + labs(title="Cosine between true and estimated exposures")
 
 
+pdf(file = "~/GitHub/simbasilica/nobuild/simulations/run_newmodel_0806.pdf",
+    height = 8, width = 8)
+mse_counts %>% print()
+mse_expos %>% print()
+cos_sigs %>% print()
+cos_expos %>% print()
+dev.off()
 
 
-## Example ####
-simul_id = "N50.G2.s4"
+## Example good ####
+simul_id1 = stats %>% filter(mse_expos == min(stats$mse_expos)) %>% dplyr::pull(idd)
+
+x.simul1 = readRDS(paste0(data_path, "simul.", simul_id1, ".Rds")) %>%
+  create_basilica_obj_simul()
+x.fit1 = readRDS(paste0(fits_path, "fit.", simul_id1, ".Rds"))
+x.fit.hier1 = readRDS(paste0(fits_path, "fit.hier.", simul_id1, ".Rds"))
+
+assigned.fit1 = compare_sigs_inf_gt(get_signatures(x.fit1), get_signatures(x.simul1), cutoff=0.8)
+unassigned.fit1 = c(setdiff(rownames(get_signatures(x.fit1)), assigned.fit1),
+                    setdiff(rownames(get_signatures(x.simul1)), names(assigned.fit1)))
+
+
+x.fit1b = x.fit1$fit$secondBest %>% create_basilica_obj()
+plot_similarity_reference(x.fit1, reference = x.simul1 %>% get_signatures())
+plot_similarity_reference(x.fit1b, reference = x.simul1 %>% get_signatures())
+
+plot1 = plot_fit(x.fit1)
+plot2 = plot_fit(x.fit.hier1)
+plot_similarity_reference(x.fit1, reference = get_signatures(x.simul1))
+
+
+
+plot_mutations(x.simul1)
+plot_exposures(x.simul1)
+plot_signatures(x.simul1)
+
+plot_signatures(x.fit1)
+plot_signatures(x.fit.hier1)
+
+
+## Example bad ####
+simul_id = stats %>% filter(mse_expos == max(stats$mse_expos)) %>% dplyr::pull(idd)
+# simul_id = "N50.G2.s4"
 
 x.simul = readRDS(paste0(data_path, "simul.", simul_id, ".Rds")) %>%
   create_basilica_obj_simul()
 x.fit = readRDS(paste0(fits_path, "fit.", simul_id, ".Rds"))
 x.fit.hier = readRDS(paste0(fits_path, "fit.", simul_id, ".Rds"))
-
-x.fit2 = two_steps_inference(x=x.simul$input$counts,
-                             k=0:10, py=py, reg_weight=0.,
-                             reference_catalogue=COSMIC_filt_merged, )
 
 
 plot_fit(x.simul)
