240cd9227871541181438572c5f40b2a17d60205
elena-buscaroli <ele.buscaroli@gmail.com>
2023-06-17 09:58:53 +0200

    adding logs and sbatch scripts

--- nobuild/poster_bits/fits_gel/fits_gel.R
+++ nobuild/poster_bits/fits_gel/fits_gel.R
@@ -1,4 +1,9 @@
-base_path = "~/GitHub/"
+args = commandArgs(trailingOnly = TRUE)
+i = as.integer(args[1])
+
+print(i)
+
+base_path = "/home/elena.buscaroli/GitHub/"
 devtools::load_all(paste0(base_path, "simbasilica"))
 load_deps(base_path=base_path)
 
@@ -9,14 +14,17 @@ counts = readRDS(paste0(base_path, "simbasilica/nobuild/poster_bits/fits_gel/inp
   dplyr::filter(cohort == "GEL", organ %in% tissues) %>% dplyr::select(-cohort, -organ)
 
 groups = readRDS(paste0(base_path, "simbasilica/nobuild/poster_bits/fits_gel/input_data.rds")) %>%
-  dplyr::filter(cohort == "GEL", organ %in% tissues) %>% dplyr::select(organ) %>%
+  dplyr::filter(cohort == "GEL", organ %in% tissues) %>% dplyr::pull(organ) %>%
   map_groups()
 
-fit.nh = two_steps_inference(x=counts, k=0:15, groups=NULL, seed_list=c(12,22,43,55))
-saveRDS(fit.nh, paste0(out_path, "fit_gel.nh.Rds"))
 
-fit.h = two_steps_inference(x=counts, k=0:15, groups=groups, seed_list=c(12,22,43,55))
-saveRDS(fit.h, paste0(out_path, "fit_gel.h.Rds"))
+if (i == 0) {
+  fit.nh = two_steps_inference(x=counts, k=0:15, groups=NULL, seed_list=c(12,22,43,55), CUDA=TRUE)
+  saveRDS(fit.nh, paste0(out_path, "fit_gel.BR_CRC_L.nh.Rds"))
+} else if (i == 1) {
+  fit.h = two_steps_inference(x=counts, k=0:15, groups=groups, seed_list=c(12,22,43,55), CUDA=TRUE)
+  saveRDS(fit.h, paste0(out_path, "fit_gel.BR_CRC_L.h.Rds"))
+}
 
 
 
