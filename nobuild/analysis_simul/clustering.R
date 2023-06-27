31b8b17605bb842ae1061e76c1210664b5b0d0ee
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-06-27 15:15:03 +0200

    mc

---
+++ nobuild/analysis_simul/clustering.R
@@ -0,0 +1,22 @@
+devtools::load_all()
+load_deps()
+data_path ="~/GitHub/simbasilica/nobuild/simulations/synthetic_datasets_1606/"
+
+simul = readRDS(list.files(data_path, full.names=T, pattern="N50")[1]) %>% create_basilica_obj_simul()
+
+fit.cl = two_steps_inference(x=get_data(simul),
+                             k=0:10, clusters=2,
+                             reference_catalogue=COSMIC_filt_merged[c("SBS1","SBS5"),],
+                             py=py, new_hier=TRUE)
+
+fit.cl2 = two_steps_inference(x=get_data(simul),
+                             k=0:10, clusters=2,
+                             reference_catalogue=COSMIC_filt_merged[c("SBS1","SBS5"),],
+                             py=py, new_hier=TRUE)
+
+fit.cl %>% convert_sigs_names(simul) %>%
+  plot_fit(x.true=simul,
+           cls=gen_palette(n=6) %>%
+             setNames(get_signames(simul)),
+           sample_name=T, sampleIDs=5)
+
---
+++ nobuild/analysis_simul/evaluate_simuls.R
@@ -0,0 +1,28 @@
+devtools::load_all()
+load_deps()
+
+data_path ="~/GitHub/simbasilica/nobuild/simulations/synthetic_datasets_1606/"
+# fits_path = "~/GitHub/simbasilica/nobuild/poster_bits/run_new_model_wholeCat_1606/"
+fits_path = "~/GitHub/simbasilica/nobuild/simulations/run_newmodel_2606_dn/"
+
+
+out_name = "dn_2606"
+
+stats_df = get_stats_df(data_path=data_path, fits_path=fits_path, cutoff=0.8)
+
+plot_sigs_stats(stats_df, wrap=T)
+plot_sigs_stats(stats_df, wrap=T, ratio=T)
+plot_metrics_stats(stats_df, wrap=T)
+# plot_mse_cosine(stats_df, colname="cosine_expos_rare")  # probably error
+plot_mse_cosine(stats_df, colname="mse_expos_rare")
+
+
+
+## example
+
+fitname = list.files(fits_path, pattern="fit.")[1]
+x.simul = readRDS(paste0(data_path, fitname %>% stringr::str_replace_all("fit.hier.","simul."))) %>%
+  create_basilica_obj_simul()
+x.fit = readRDS(paste0(fits_path, fitname)) %>% convert_sigs_names(x.simul)
+
+
