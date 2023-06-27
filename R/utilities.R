c237894ada6dde412dc2ccecf1acd0b244f1b7f7
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-06-27 10:28:18 +0200

    mc

--- R/utilities.R
+++ R/utilities.R
@@ -117,6 +117,58 @@ compute.mse <- function(m_inf, m_true, subset_cols=NULL, assigned_missing=NULL) 
 }
 
 
+
+compute.cosine = function(m1, m2, assigned_missing, what, subset_cols=NULL) {
+  # m1 = fit
+  # m2 = simul
+
+  unassigned = unique(c(assigned_missing$missing_fn,
+                        assigned_missing$added_fp))
+
+  if (what == "expos") {
+    m1 = as.data.frame(t(m1))
+    m2 = as.data.frame(t(m2))
+  }
+
+  if (!is.null(subset_cols)) {
+    m1 = m1[intersect(rownames(m1), assigned_missing$assigned_tp[subset_cols]), ]
+    m2 = m2[intersect(rownames(m2), subset_cols), ]
+
+    if (nrow(m1) == 0) {
+      return(0)
+    }
+    tmp = intersect(subset_cols, names(assigned_missing$assigned_tp))
+    consider = assigned_missing$assigned_tp[tmp] %>%
+      setNames(tmp)
+    unassigned = intersect(unassigned, subset_cols)
+  } else { consider = assigned_missing$assigned_tp }
+
+  rownames(m1) = paste0("F_", rownames(m1))
+  rownames(m2) = paste0("S_", rownames(m2))
+
+  compare = rbind(m1[paste0("F_",consider), ],
+                  m2[paste0("S_",names(consider)), ] )
+
+  cosine_sim = lsa::cosine(t(compare))[paste0("F_",consider),
+                                       paste0("S_",names(consider))]
+
+  if (is.null(dim(cosine_sim)))
+    cosines_tmp = cosine_sim
+  else
+    cosines_tmp = sapply(names(consider), function(i) {
+      cosine_sim[paste0("F_",consider[i]), paste0("S_",i)]
+    })
+
+  return(mean(cosines_tmp))
+
+  # return(
+  #   mean(c(cosines_tmp,
+  #          rep(0, length.out=length(unassigned))))
+  # )
+}
+
+
+
 denovo.similarity <- function(expected_denovo, inferred_denovo) {
 
   if (length(expected_denovo)==0 | length(inferred_denovo)==0) {
