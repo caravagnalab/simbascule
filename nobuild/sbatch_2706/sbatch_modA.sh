b4f333f42e00f44fe5d3d9db1867a51baa61001b
elena-buscaroli <ele.buscaroli@gmail.com>
2023-06-30 00:00:29 +0200

    adding simuls

---
+++ nobuild/sbatch_2706/sbatch_modA.sh
@@ -0,0 +1,7 @@
+sbatch 1.modA_cosine.sbatch
+sbatch 1.modA_noreg.sbatch
+
+sbatch 2.modA_cosine.sbatch
+sbatch 2.modA_noreg.sbatch
+
+
---
+++ nobuild/sbatch_2706/sbatch_modB.sh
@@ -0,0 +1,7 @@
+sbatch 1.modB_cosine.sbatch
+sbatch 1.modB_noreg.sbatch
+
+sbatch 2.modB_cosine.sbatch
+sbatch 2.modB_noreg.sbatch
+
+
