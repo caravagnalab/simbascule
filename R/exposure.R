58215bd8885b97796afc654b1e2324f758d8001f
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-07-05 15:06:00 +0200

    adding new dataset

--- R/exposure.R
+++ R/exposure.R
@@ -1,7 +1,10 @@
 library(gtools)
 library(fdrtool)
 
-generate.exposure <- function(beta, groups, private_sigs, private_fracs, seed=NULL, thr=0.1) {
+generate.exposure <- function(beta, groups, private_sigs, private_fracs,
+                              n_priv_per_group=1, seed=NULL, thr=0.1) {
+
+  if (is.null(n_priv_per_group)) n_priv_per_group = round(length(private_sigs.all)/2)
 
   signatures <- rownames(beta)
   if (!('SBS1' %in% signatures)) stop('Wrong signatures! SBS1 not included!')
@@ -12,7 +15,7 @@ generate.exposure <- function(beta, groups, private_sigs, private_fracs, seed=NU
 
   df_list <- list()
 
-  private_sigs.all = c(private_sigs$common, private_sigs$rare)
+  private_sigs.all = private_sigs.start = c(private_sigs$common, private_sigs$rare)
   shared_sigs = setdiff(signatures, private_sigs.all)
 
   unq = unique(groups) %>% sort()
@@ -36,11 +39,15 @@ generate.exposure <- function(beta, groups, private_sigs, private_fracs, seed=NU
       if (length(private_sigs.all)>0) {
         if (i == length(unq))
           sigNums_priv = length(private_sigs.all) else
-          sigNums_priv = base::sample(1:round(length(private_sigs.all)/2), 1)
+          sigNums_priv = base::sample(1:n_priv_per_group, 1)
 
         sigNames_priv = base::sample(private_sigs.all, sigNums_priv)
-        private_sigs.all = setdiff(private_sigs.all, sigNames_priv)
-        # print(paste0("signames_priv", sigNames_priv))
+
+        sigNames_priv = c(sigNames_priv, private_sigs.start[sigNames_priv],
+                          private_sigs.start[private_sigs.start==sigNames_priv] %>% names) %>%
+          purrr::discard(function(x) x=="" || is.na(x)) %>% unique()
+
+        private_sigs.all = private_sigs.all %>% purrr::discard(function(x) x %in% sigNames_priv)
 
         sigNames = c(shared_sigs, sigNames_priv)
         sigNums = length(sigNames)
@@ -58,15 +65,24 @@ generate.exposure <- function(beta, groups, private_sigs, private_fracs, seed=NU
 
     ## change values for rare/common
 
-    alpha = adjust_frequency(alpha,
+    alpha = adjust_frequency(alpha, N=length(groups),
                      columns=intersect(private_sigs$rare, colnames(alpha)),
                      frac=private_fracs$rare, check=">=",
-                     mean=rep(0,length.out=sigNums) %>% setNames(sigNames), sd=0)
+                     # mean=rep(0, length.out=length(intersect(private_sigs$rare, colnames(alpha)))) %>%
+                     #   setNames(intersect(private_sigs$rare, colnames(alpha))),
+                     mean=rep(0, length.out=ncol(alpha)) %>% setNames(colnames(alpha)),
+                     sd=0, min_exp=0.3)
 
-    alpha = adjust_frequency(alpha,
+    alpha = adjust_frequency(alpha, N=length(groups),
                      columns=intersect(private_sigs$common, colnames(alpha)),
-                     frac=private_fracs$common, check="<=",
-                     mean=mean_prior, sd=1, thr=thr)
+                     frac=private_fracs$common, check="<",
+                     mean=mean_prior, sd=1, thr=thr, min_exp=0.1)
+
+    alpha = adjust_frequency(alpha, N=length(groups),
+                             columns=shared_sigs,
+                             frac=nrow(alpha), check="<",
+                             mean=mean_prior,
+                             sd=1, thr=thr, min_exp=0.1)
 
     alpha = apply(alpha, 1, function(x) x/sum(x)) %>% t() %>% as.data.frame()
 
@@ -95,15 +111,16 @@ generate.exposure <- function(beta, groups, private_sigs, private_fracs, seed=NU
   }
 
 
-adjust_frequency = function(alpha, columns, frac, check, mean, sd, thr=0.) {
-  if (!is.integer(frac)) { nn = round(nrow(alpha) * frac) }
-  else { nn = frac}
+adjust_frequency = function(alpha, N, columns, frac, check, mean, sd, thr=0., min_exp=0.2) {
+  if (!is.integer(frac)) { nn = min(nrow(alpha), round(N * frac)) } else { nn = frac}
   for (colname in columns) {
     alpha = alpha %>% tibble::as_tibble() %>% dplyr::mutate(!!colname:=replace( .[[colname]], {{colname}}<thr, 0 ))
 
-    if (!match.fun(check)(sum(alpha[,colname]>0), nn) ) next
+    if ( !match.fun(check)(sum(alpha[,colname]>0), nn) ) next
     samples.tmp = sample(1:nrow(alpha), size=nn)
     alpha[-samples.tmp, colname] = sample_positive_norm(mean[colname], sd)
+    while (any(alpha[samples.tmp, colname] < min_exp))
+      alpha[samples.tmp, colname] = sample_positive_norm(mean[colname], sd=1)
   }
 
   return(alpha)
