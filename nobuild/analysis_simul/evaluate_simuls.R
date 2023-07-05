292dc1d9e8278414bd949ae36e625bec4282c72b
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-07-05 10:12:19 +0200

    mc

--- nobuild/analysis_simul/evaluate_simuls.R
+++ nobuild/analysis_simul/evaluate_simuls.R
@@ -10,38 +10,44 @@ fits_path1 = c("~/GitHub/simbasilica/nobuild/simulations/fits_dn_1606.cosine.old
 fits_path2 = c("~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.cosine.new_hier/",
                "~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.cosine.old_hier/",
                "~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.noreg.new_hier/",
-               "~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.noreg.old_hier/")
+               "~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.noreg.old_hier/",
+               "~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.KL.new_hier/",
+               "~/GitHub/simbasilica/nobuild/simulations/fits_dn.hier_clust.KL.old_hier/")
 
-run_id1 = c("cosine", "noreg") %>% setNames(fits_path1)
-run_id2 = c("cosine.new", "cosine.old", "noreg.new", "noreg.old") %>%
+run_id1 = c("cosine.nohier", "noreg.nohier") %>% setNames(fits_path1)
+run_id2 = c("cosine.new", "cosine.old", "noreg.new", "noreg.old", "KL.new", "KL.old") %>%
   setNames(fits_path2)
 
-cutoff = 0.8
+cutoff = 0.6
 stats_df = get_stats_df(data_path=data_path, fits_path=fits_path1, cutoff=cutoff, fits_pattern=c("fit.")) %>%
   dplyr::mutate(run_id=run_id1[fits_path]) %>%
 
   dplyr::add_row(
     get_stats_df(data_path=data_path, fits_path=fits_path2, cutoff=cutoff, fits_pattern=c("fit_hier.")) %>%
-      dplyr::mutate(run_id=run_id[fits_path])
+      dplyr::mutate(run_id=run_id2[fits_path])
   ) %>%
 
   dplyr::add_row(
     get_stats_df(data_path=data_path, fits_path=fits_path2, cutoff=cutoff, fits_pattern=c("fit_clust.")) %>%
       dplyr::mutate(run_id=run_id2[fits_path])
   ) %>%
-  dplyr::mutate(unique_id=paste(inf_type, run_id, sep="."))
-
-
-stats_df %>%
+  dplyr::mutate(unique_id=paste(inf_type, run_id, sep=".")) %>%
   dplyr::mutate(regularizer=dplyr::case_when(
     grepl("cosine", run_id) ~ "cosine",
     grepl("KL", run_id) ~ "KL",
     grepl("noreg", run_id) ~ "noreg"
   ), model=dplyr::case_when(
-    grepl("old_hier", fits_path) ~ "old_hier",
-    grepl("new_hier", fits_path) ~ "new_hier",
-    .default = ""
-  )) %>%
+    grepl("old", run_id) ~ "old_hier",
+    grepl("new", run_id) ~ "new_hier",
+    grepl("nohier", run_id) ~ "no_hier",
+  ))
+
+saveRDS(stats_df, paste0(save_path, "stats_df.sim", cutoff*100, ".0407.Rds"))
+
+
+
+pdf(paste0(save_path, "stats_report.sim", cutoff*100, ".pdf"), height=6, width=10)
+stats_df %>%
   dplyr::mutate(ratio = n_sigs_found / n_sigs) %>%
   ggplot() +
   geom_hline(yintercept=1, linetype="dashed", linewidth=0.5, color="grey") +
@@ -50,21 +56,150 @@ stats_df %>%
 
   geom_jitter(aes(x=as.factor(N), y=ratio, color=inf_type),
               position=position_jitterdodge(jitter.width=0.05,
-                                            jitter.height=0.05,
+                                            jitter.height=0,
                                             dodge.width=0.7),
               size=0.8) +
   # scale_color_manual(values=cols, name="") +
   # scale_fill_manual(values=cols, name="") +
   theme_bw() + xlab("# samples") + ylab("Ratio") +
-  # labs(title=paste0("Ratio between ", tolower(axis_lab), " found and true")) +
-  ylim(0-0.01, NA) +
-  ggh4x::facet_nested(G ~ regularizer + model)
+  labs(title=paste0("Ratio between signatures found and true")) +
+  # ylim(0-0.01, NA) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
+
+
+stats_df %>%
+  dplyr::mutate(ratio = n_common_found / n_common) %>%
+  ggplot() +
+  geom_hline(yintercept=1, linetype="dashed", linewidth=0.5, color="grey") +
+  geom_violin(aes(x=as.factor(N), y=ratio, fill=inf_type),
+              alpha=0.5, position=position_dodge(width=0.7), color=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=ratio, color=inf_type),
+              position=position_jitterdodge(jitter.width=0.05,
+                                            jitter.height=0,
+                                            dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("Ratio") +
+  labs(title=paste0("Ratio between common signatures found and true")) +
+  # ylim(0-0.01, NA) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
+
+
+stats_df %>%
+  dplyr::mutate(ratio = n_rare_found / n_rare) %>%
+  ggplot() +
+  geom_hline(yintercept=1, linetype="dashed", linewidth=0.5, color="grey") +
+  geom_violin(aes(x=as.factor(N), y=ratio, fill=inf_type),
+              alpha=0.5, position=position_dodge(width=0.7), color=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=ratio, color=inf_type),
+              position=position_jitterdodge(jitter.width=0.05,
+                                            jitter.height=0,
+                                            dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("Ratio") +
+  labs(title=paste0("Ratio between rare signatures found and true")) +
+  # ylim(0-0.1, NA) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
+
+
+stats_df %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.factor(N), y=mse_counts, color=inf_type),
+               alpha=0.5, position=position_dodge(width=0.7), outlier.colour=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=mse_counts, color=inf_type),
+              position=position_jitterdodge(jitter.width=0.01,
+                                            jitter.height=0,
+                                            dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("MSE") +
+  labs(title=paste0("MSE counts")) +
+  # ylim(-0.01, 1) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
+
+stats_df %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.factor(N), y=mse_expos, color=inf_type),
+               alpha=0.5, position=position_dodge(width=0.7), outlier.colour=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=mse_expos, color=inf_type),
+              position=position_jitterdodge(jitter.width=0.01,
+                                            jitter.height=0,
+                                            dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("MSE") +
+  labs(title=paste0("MSE exposures")) +
+  # ylim(-0.01, 1) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
+
+
+stats_df %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.factor(N), y=cosine_sigs, color=inf_type),
+               alpha=0.5, position=position_dodge(width=0.7), outlier.colour=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=cosine_sigs, color=inf_type),
+              position=position_jitterdodge(jitter.width=0.01,
+                                            jitter.height=0,
+                                            dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("Cosine") +
+  labs(title=paste0("Cosine signatures")) +
+  # ylim(-0.01, 1) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
+
+
+stats_df %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.factor(N), y=cosine_expos, color=inf_type),
+               alpha=0.5, position=position_dodge(width=0.7), outlier.colour=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=cosine_expos, color=inf_type),
+              position=position_jitterdodge(jitter.width=0.01,
+                                            jitter.height=0,
+                                            dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("Cosine") +
+  labs(title=paste0("Cosine exposures")) +
+  # ylim(-0.01, 1) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
 
 
 
-saveRDS(stats_df, paste0(save_path, ""))
+stats_df %>%
+  dplyr::filter(inf_type == "fit_clust") %>%
+  dplyr::mutate(ratio = n_groups_found / G) %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.factor(N), y=ratio),
+               alpha=0.5, position=position_dodge(width=0.7), outlier.colour=NA) +
+
+  geom_jitter(aes(x=as.factor(N), y=ratio), height = 0,
+              # position=position_jitterdodge(jitter.width=0.01,
+              #                               jitter.height=0.01,
+              #                               dodge.width=0.7),
+              size=0.8) +
+  # scale_color_manual(values=cols, name="") +
+  # scale_fill_manual(values=cols, name="") +
+  theme_bw() + xlab("# samples") + ylab("Ratio") +
+  labs(title=paste0("N groups found")) +
+  # ylim(-0.01, 1) +
+  ggh4x::facet_nested(G ~ regularizer + model, scales="free_y")
 
 
+dev.off()
 
 
 # stats_df = get_stats_df(data_path=data_path, fits_path=fits_path, cutoff=0.8, fits_pattern=fits_pattern) %>%
