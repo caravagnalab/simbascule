c7d5911c9efd57af2f2977c29458f9849b911039
elena-buscaroli <ele.buscaroli@gmail.com>
2023-07-06 09:32:50 +0200

    adding simulations new datasets

--- nobuild/run_scripts/run_dn.R
+++ nobuild/run_scripts/run_dn.R
@@ -9,12 +9,14 @@ if (regularizer == "noreg") {
   reg_weight = 0.
   } else reg_weight = 1.
 
+run_id = args[5]
+
 cat(paste("i =", i, "new_hier =", new_hier, "regularizer =", regularizer, "inference_type =", inference_type, "\n"))
 
 main_path = "/home/elena.buscaroli/GitHub/"
-data_path = paste0(main_path, "simbasilica/nobuild/simulations/synthetic_datasets_1606/")
+data_path = paste0(main_path, "simbasilica/nobuild/simulations/synthetic_datasets_0507/")
 fits_path = paste0(main_path, "simbasilica/nobuild/simulations/", "fits_dn.", 
-       inference_type, ".", regularizer, ".", args[2], "/")
+       inference_type, ".", regularizer, ".", args[2], ".", run_id, "/")
 
 cat(paste0("\nSaving in directory: ", fits_path, "\n\n"))
 
@@ -32,24 +34,40 @@ cli::cli_process_done()
 
 
 # Matrix with combs #####
-comb = tibble(
-  N_vals = c(50, 300, 300, 1000, 1000, 5000, 5000),
-  n_groups_vals = c(2, 2, 4, 4, 6, 6, 10),
-  samples_per_group = list(25:50, 50:300, 50:300, 100:1000, 100:1000, 200:5000, 200:5000),
-  n_priv_comm = c(2, 2, 4, 4, 5, 5, 5),
-  n_priv_rare = c(1, 1, 1, 1, 2, 2, 3)
-)
+# comb = tibble(
+#   N_vals = c(50, 300, 300, 1000, 1000, 5000, 5000),
+#   n_groups_vals = c(2, 2, 4, 4, 6, 6, 10),
+#   samples_per_group = list(25:50, 50:300, 50:300, 100:1000, 100:1000, 200:5000, 200:5000),
+#   n_priv_comm = c(2, 2, 4, 4, 5, 5, 5),
+#   n_priv_rare = c(1, 1, 1, 1, 2, 2, 3)
+# )
 
-shared = c("SBS1", "SBS5", "SBS17b")
-private = c("SBS10b", "SBS28", "SBS56 SBS10a", "SBS90", "SBS2", "SBS13", "SBS20", "SBS22")
+# shared = c("SBS1", "SBS5", "SBS17b")
+# private = c("SBS10b", "SBS28", "SBS56 SBS10a", "SBS90", "SBS2", "SBS13", "SBS20", "SBS22")
+
+N = c(150, 500, 1000, 5000)
+G = c(1, 3, 5, 5)
+samples_per_group = list(15:150, 50:500, 100:1000, 500:5000) %>% setNames(paste0("N",N))
+
+shared_sbs = c("SBS1","SBS5")
+priv_comm = paste0("SBS", c(4, 6, 13, 25))
+priv_rare = "SBS7c" %>% setNames("SBS7d")
+private = list("common"=priv_comm, "rare"=priv_rare)
+
+set.seed(1234)
+comb = tibble::tibble(N_vals=N, n_groups_vals=G) %>%
+  tidyr::complete(N_vals, n_groups_vals) %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(samples_per_group=list(samples_per_group[[paste0("N", N_vals)]]),
+                shared=list(shared_sbs), rare=list(priv_rare),
+                common=list(c("SBS7d", sample(priv_comm, size=n_groups_vals-1, replace=F))))
+
 
 comb_i = comb[i+1,]
 inference_type = c(strsplit(inference_type, "_")[[1]])
 
 # Run model #####
-generate_and_run(shared = shared,
-                 private = private,
-                 catalogue = COSMIC_filt_merged,
+generate_and_run(catalogue = COSMIC_filt_merged,
                  comb_matrix = comb_i,
                  py = py,
                  inference_type = inference_type,
@@ -57,7 +75,7 @@ generate_and_run(shared = shared,
                  fits_path = fits_path,
                  data_path = data_path,
 
-                 seeds = 1:10,
+                 seeds = 1:20,
                  mut_range = 10:8000,
                  reference_catalogue = COSMIC_filt_merged[c("SBS1","SBS5"), ],
                  input_catalogue = NULL,
