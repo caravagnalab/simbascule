0bba6f0d47c6e9123ba366808cd0671e2ade2ba0
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-08-02 16:44:29 +0200

    reorganiz

--- nobuild/sigprofiler.R
+++
@@ -1,8 +0,0 @@
-files_path = "~/GitHub/hBasilica/results/sigprofiler/simulated_data/"
-
-stats_sig = get_stats_df(data_path=data_path, fits_path=files_path, cutoff=0.8,
-                         sigprofiler=TRUE, pattern="")
-
-
-
-
