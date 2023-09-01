603cd8cb278edfd7e5916e058a1b954bccf1b601
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-09-01 12:54:03 +0200

    modifying functions for real data

--- R/utils_real_data.R
+++ R/utils_real_data.R
@@ -1,110 +1,80 @@
-get_true_expos = function(base_path, organ, long=TRUE, mod_names=FALSE) {
-  dpath = paste0("processed_data/SBS_v2.03/organSpecificExposures/GEL/GEL-",
-                 organ, "_SBS_exposures_finalT.tsv")
+get_true_expos = function(base_path, organ_names=c(), cohort_names=c("GEL","ICGC","Hartwig"),
+                          long=TRUE, mod_names=FALSE) {
+  ff = readRDS(paste0(base_path, "expos_all.Rds")) %>%
+    dplyr::select(-unassigned) %>% dplyr::filter(cohort %in% cohort_names)
 
-  ff = read.csv(paste0(base_path, dpath),
-                sep="\t", row.names=1) %>%
-    dplyr::select(-unassigned)
-
-  if (!long && !mod_names) return(ff)
-
-  if (mod_names) {
-    colnames(ff) = stringr::str_replace_all(colnames(ff),
-                                            paste0("GEL.",organ,"_common_"), "")
-    colnames(ff) = stringr::str_replace_all(colnames(ff),
-                                            paste0("GEL.",organ,"_rare_"), "")
-  }
+  if (length(organ_names) > 0) ff = ff %>% dplyr::filter(organ %in% organ_names)
 
   if (!long) return(ff)
 
   return(
     ff %>%
       tibble::rownames_to_column(var="samples") %>%
-      reshape2::melt(id="samples", variable.name="sbs", value.name="exposure") %>%
-      # tidyr::separate("sbs", into=c("rm","type","sbs"), extra="merge", sep="_") %>%
-      dplyr::mutate(organ=organ)
+      reshape2::melt(id=c("samples","cohort","organ"),
+                     variable.name="sbs", value.name="exposure") %>%
+      dplyr::mutate(sbs=as.character(sbs))
   )
 }
 
 
-
-get_true_sigs = function(base_path, organ, mod_names=F, long=T) {
-  ff = read.csv(paste0(base_path,
-                  "processed_data/SBS_v2.03/OrganSpecificSigs_GEL_SBS_v2.03.tsv"),
-           sep="\t") %>%
-    dplyr::select(dplyr::contains(organ))
-
-  if (mod_names) {
-    colnames(ff) = stringr::str_replace_all(colnames(ff),
-                                            paste0("GEL.",organ,"_common_"), "")
-    colnames(ff) = stringr::str_replace_all(colnames(ff),
-                                            paste0("GEL.",organ,"_rare_"), "")
-  }
+get_true_sigs = function(base_path, mod_names=F, long=T) {
+  ff = readRDS(paste0(base_path, "signatures_all.Rds"))
 
   if (long)
     ff = ff %>%
-      tibble::rownames_to_column(var="contexts") %>%
-      reshape2::melt(id="contexts", variable.name="sbs", value.name="beta") %>%
-      dplyr::mutate(organ=organ)
+      tibble::rownames_to_column(var="sbs") %>%
+      reshape2::melt(id="sbs", value.name="beta")
 
   return(ff)
 }
 
 
-
 create_basilica_obj_real_data = function(base_path, counts, groupids) {
-  sampleids = counts %>% rownames
+  sampleids = counts %>% rownames()
 
-  true_expos = get_true_expos(base_path, "Lung", mod_names=T) %>%
-    rbind(get_true_expos(base_path, "Colorectal", mod_names=T)) %>%
+  true_expos = get_true_expos(base_path=base_path, long=T) %>%
     dplyr::filter(samples %in% sampleids) %>%
-    dplyr::group_by(samples, organ) %>%
-    dplyr::mutate(exposure=exposure / sum(exposure)) %>% dplyr::ungroup()
+    dplyr::group_by(samples, organ, cohort) %>%
+    dplyr::mutate(exposure=exposure / sum(exposure)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(idd=paste(cohort, organ, sep="_"))
 
-  true_signames = true_expos$sbs %>% as.character() %>% unique()
-  lung_signames = true_expos %>% dplyr::filter(organ=="Lung") %>% dplyr::pull(sbs) %>%
-    as.character() %>% unique()
-  crc_signames = true_expos %>% dplyr::filter(organ=="Colorectal") %>% dplyr::pull(sbs) %>%
-    as.character() %>% unique()
-  common = intersect(lung_signames, crc_signames)
+  keep_sigs = true_expos %>% dplyr::group_by(sbs) %>%
+    dplyr::summarise(keep=sum(exposure)>0) %>% dplyr::filter(keep) %>%
+    dplyr::pull(sbs)
 
-  true_sigs = get_true_sigs(base_path, "Lung", mod_names=T) %>%
-    rbind(get_true_sigs(base_path, "Colorectal", mod_names=T)) %>%
-    dplyr::filter(sbs %in% true_signames)
+  groups = true_expos %>% dplyr::select(samples, idd) %>% unique()
+  groups = groups$idd %>% setNames(groups$samples)
 
-  true_sigs_wide = get_true_sigs(base_path, "Lung", mod_names=F, long=F) %>%
-    cbind(get_true_sigs(base_path, "Colorectal", mod_names=F, long=F)) %>%
-    dplyr::select(dplyr::contains(true_signames))
+  true_sigs = get_true_sigs(base_path=base_path, long=F)[keep_sigs, ]
 
+  # COSMIC = renormalize_denovo_thr(COSMIC_catalogue)
+  # non_ref = setdiff(true_signames, rownames(COSMIC))
+  # ref = intersect(rownames(COSMIC), true_signames)
+  # non_ref_common = intersect(non_ref, common)
+  # non_ref_unique = setdiff(non_ref, common)
 
-  COSMIC = renormalize_denovo_thr(COSMIC_catalogue)
-  non_ref = setdiff(true_signames, rownames(COSMIC))
-  ref = intersect(rownames(COSMIC), true_signames)
-  non_ref_common = intersect(non_ref, common)
-  non_ref_unique = setdiff(non_ref, common)
+  # unique_nonref_cat = true_sigs_wide %>%
+  #   dplyr::select(dplyr::contains(non_ref_unique)) %>%
+  #   t() %>% as.data.frame() %>% tibble::rownames_to_column(var="sbs") %>%
+  #   tidyr::separate("sbs", into=c("rm1","rm2","sbs"), sep="_", extra="merge") %>%
+  #   dplyr::select(-dplyr::contains("rm")) %>% tibble::column_to_rownames("sbs")
+  #
+  # common_nonref_cat = true_sigs_wide %>%
+  #   dplyr::select(dplyr::contains(non_ref_common)) %>% t() %>%
+  #   as.data.frame() %>% tibble::rownames_to_column(var="sbs") %>%
+  #   reshape2::melt(id="sbs", variable.name="contexts", value.name="beta") %>%
+  #   tidyr::separate("sbs", into=c("rm1","rm2","sbs"), sep="_", extra="merge") %>%
+  #   dplyr::select(-dplyr::contains("rm")) %>%
+  #   dplyr::group_by(sbs, contexts) %>%
+  #   dplyr::reframe(beta=mean(beta)) %>% dplyr::ungroup() %>%
+  #   tidyr::pivot_wider(id_cols="sbs", values_from="beta", names_from="contexts") %>%
+  #   tibble::column_to_rownames("sbs")
 
-  unique_nonref_cat = true_sigs_wide %>%
-    dplyr::select(dplyr::contains(non_ref_unique)) %>%
-    t() %>% as.data.frame() %>% tibble::rownames_to_column(var="sbs") %>%
-    tidyr::separate("sbs", into=c("rm1","rm2","sbs"), sep="_", extra="merge") %>%
-    dplyr::select(-dplyr::contains("rm")) %>% tibble::column_to_rownames("sbs")
-
-  common_nonref_cat = true_sigs_wide %>%
-    dplyr::select(dplyr::contains(non_ref_common)) %>% t() %>%
-    as.data.frame() %>% tibble::rownames_to_column(var="sbs") %>%
-    reshape2::melt(id="sbs", variable.name="contexts", value.name="beta") %>%
-    tidyr::separate("sbs", into=c("rm1","rm2","sbs"), sep="_", extra="merge") %>%
-    dplyr::select(-dplyr::contains("rm")) %>%
-    dplyr::group_by(sbs, contexts) %>%
-    dplyr::reframe(beta=mean(beta)) %>% dplyr::ungroup() %>%
-    tidyr::pivot_wider(id_cols="sbs", values_from="beta", names_from="contexts") %>%
-    tibble::column_to_rownames("sbs")
-
-  catalogue = rbind(COSMIC[ref,], unique_nonref_cat, common_nonref_cat) %>%
-    renormalize_denovo_thr()
-  catalogue["SBS5",] = COSMIC_catalogue["SBS5",]
-
-  catalogue_filt = catalogue[!grepl("\\.|\\_", rownames(catalogue)),]
+  # catalogue = rbind(COSMIC[ref,], unique_nonref_cat, common_nonref_cat) %>%
+  #   renormalize_denovo_thr()
+  # catalogue["SBS5",] = COSMIC_catalogue["SBS5",]
+  # catalogue_filt = catalogue[!grepl("\\.|\\_", rownames(catalogue)),]
 
   true_expos_wide = true_expos %>%
     dplyr::mutate(exposure=replace(exposure, is.na(exposure), 0)) %>%
@@ -128,9 +98,6 @@ create_basilica_obj_real_data = function(base_path, counts, groupids) {
 
 
 
-
-
-
 create_basilica_obj_sigprofiler = function(fitname, counts=NULL) {
   sigs = read.csv(paste0(fitname, "/SBS96/Suggested_Solution/COSMIC_SBS96_Decomposed_Solution/Signatures/COSMIC_SBS96_Signatures.txt"), sep="\t", row.names = 1) %>%
     t() %>% as.data.frame()
@@ -138,7 +105,6 @@ create_basilica_obj_sigprofiler = function(fitname, counts=NULL) {
   expos = read.csv(paste0(fitname, "/SBS96/Suggested_Solution/COSMIC_SBS96_Decomposed_Solution/Activities/COSMIC_SBS96_Activities.txt"), sep="\t", row.names = 1) %>%
     as.data.frame()
   expos = expos / rowSums(expos)
-
 
   obj = list(); class(obj) = "basilica_obj"
 
@@ -150,6 +116,54 @@ create_basilica_obj_sigprofiler = function(fitname, counts=NULL) {
   obj$fit$exposure = expos
 
   return(obj)
+}
+
+
+
+
+get_true_expos_old = function(base_path, organ, long=TRUE, mod_names=FALSE) {
+  dpath = paste0("processed_data/SBS_v2.03/organSpecificExposures/GEL/GEL-",
+                 organ, "_SBS_exposures_finalT.tsv")
+
+  ff = read.csv(paste0(base_path, dpath), sep="\t", row.names=1) %>%
+    dplyr::select(-unassigned)
+
+  if (!long && !mod_names) return(ff)
+
+  if (mod_names) {
+    colnames(ff) = stringr::str_replace_all(colnames(ff), paste0("GEL.",organ,"_common_"), "")
+    colnames(ff) = stringr::str_replace_all(colnames(ff), paste0("GEL.",organ,"_rare_"), "")
+  }
+
+  if (!long) return(ff)
+
+  return(
+    ff %>%
+      tibble::rownames_to_column(var="samples") %>%
+      reshape2::melt(id="samples", variable.name="sbs", value.name="exposure") %>%
+      # tidyr::separate("sbs", into=c("rm","type","sbs"), extra="merge", sep="_") %>%
+      dplyr::mutate(organ=organ)
+  )
+}
+
+
+get_true_sigs_old = function(base_path, organ, mod_names=F, long=T) {
+  ff = read.csv(paste0(base_path,
+                       "processed_data/SBS_v2.03/OrganSpecificSigs_GEL_SBS_v2.03.tsv"),
+                sep="\t") %>%
+    dplyr::select(dplyr::contains(organ))
+
+  if (mod_names) {
+    colnames(ff) = stringr::str_replace_all(colnames(ff), paste0("GEL.",organ,"_common_"), "")
+    colnames(ff) = stringr::str_replace_all(colnames(ff), paste0("GEL.",organ,"_rare_"), "")
+  }
+
+  if (long)
+    ff = ff %>%
+      tibble::rownames_to_column(var="contexts") %>%
+      reshape2::melt(id="contexts", variable.name="sbs", value.name="beta")
+
+  return(ff)
 }
 
 
--- nobuild/analysis_real_data/evaluate_real_data.R
+++ nobuild/analysis_real_data/evaluate_real_data.R
@@ -18,11 +18,41 @@ groups_n = groups_all[idxs]
 
 
 fit_dn = fit(x=counts_n, k=0:15, clusters=10, nonparametric=TRUE, keep_sigs=c("SBS1","SBS5"),
-             reference_catalogue=COSMIC_filt[c("SBS1","SBS5"),], reg_weight=0.)
+             reference_catalogue=COSMIC_filt[c("SBS1","SBS5"),], reg_weight=0., verbose=T)
 fit_cat = fit(x=counts_n, k=0:15, clusters=10, nonparametric=TRUE, keep_sigs=c("SBS1","SBS5"),
-              reference_catalogue=COSMIC_filt, reg_weight=0.)
+              reference_catalogue=COSMIC_filt, reg_weight=0., verbose=T)
+
+fit_dn2 = fit(x=counts_n, k=10:15, clusters=15, nonparametric=TRUE, keep_sigs=c("SBS1","SBS5"),
+              reference_catalogue=COSMIC_filt[c("SBS1","SBS5"),], reg_weight=0., verbose=T,
+              hyperparameters = list("alpha_sigma"=0.025))
+
+fit_dn2 %>% filter_exposures(0.05) %>%
+  plot_exposures() %>%
+  patchwork::wrap_plots(fit_dn2 %>% filter_exposures(0.05) %>%
+                          plot_exposures(centroids=T),
+                        ncol=2, widths=c(7,1), guides="collect") & theme(legend.position="bottom")
+
+# fix_assignments(fit_dn) %>% plot_exposures()
+saveRDS(fit_dn, paste0(save_path, "fit_CRC_dn.Rds"))
+saveRDS(fit_cat, paste0(save_path, "fit_CRC_cat.Rds"))
+
+fit_dn = readRDS(paste0(save_path, "fit_CRC_dn.Rds"))
+fit_cat = readRDS(paste0(save_path, "fit_CRC_cat.Rds"))
+
+samples = get_group(fit_dn, groupIDs = "7", return_idx = T)
+fit_dn %>% convert_sigs_names(reference_cat=COSMIC_filt) %>%
+  filter_exposures() %>% plot_exposures() %>%
+  patchwork::wrap_plots(fit_dn %>% convert_sigs_names(reference_cat=COSMIC_filt) %>%
+                          plot_exposures(sampleIDs=samples, centroids = T))
+fit_dn %>% recompute_centroids() %>% merge_clusters() %>% plot_exposures()
 
 
+
+
+fit_cat_mod = fit_cat %>% convert_sigs_names(reference_cat = COSMIC_filt_merged) %>%
+  recompute_centroids() %>% merge_clusters()
+
+filter_signatures_QP(get_signatures(fit_cat_mod), COSMIC_filt, filt_pi=0.1, return_weights=T)
 
 
 ## Generate whole count matrix ####
@@ -32,10 +62,48 @@ counts_all = lapply(catalogues, function(c_i) {
   cohort = splitted[length(splitted)-1]
   organ = strsplit(splitted[length(splitted)], "_")[[1]][2]
 
-  return(read.csv(c_i, sep="\t") %>% t() %>% as.data.frame() %>%
+  return(read.csv(c_i, sep="\t", check.names=FALSE) %>% t() %>% as.data.frame() %>%
            dplyr::mutate(organ=organ, cohort=cohort))
   }) %>% do.call(rbind, .)
 saveRDS(counts_all, file=paste0(data_path, "counts_all.Rds"))
+
+
+## Read reference signatures ####
+signatures = read.csv(paste0(data_path, "SBS_v2.03/RefSig_SBS_v2.03.tsv"), sep="\t") %>%
+  t() %>% as.data.frame()
+saveRDS(signatures, file=paste0(data_path, "signatures_all.Rds"))
+
+
+## Generate whole exposures matrix ####
+exposures = list.files(path=paste0(data_path, "SBS_v2.03/organSpecificExposures/"),
+                       recursive=TRUE, full.names=TRUE, pattern=".tsv$")
+conversion_table = read.csv(paste0(data_path, "SBS_v2.03/RefSig_SBS_conversionMatrix_v2.03.tsv"), sep="\t") %>%
+  apply(1, function(sbs) names(sbs)[sbs>0])
+referece_expos = readxl::read_xlsx(paste0(data_path, "science.abl9283_tables_s1_to_s33.v2/SupplementaryTables.xlsx"), sheet="Table S23")
+
+expos_all = lapply(exposures, function(e_i) {
+  cat(paste0(e_i, "\n"))
+  splitted = strsplit(e_i, "/")[[1]]
+  cohortname = splitted[length(splitted)-1]
+  organname = strsplit(splitted[length(splitted)], "-")[[1]][2] %>%
+    stringr::str_replace_all("_SBS_exposures_finalT.tsv","")
+
+  expos_file = read.csv(e_i, sep="\t", check.names=F) %>%
+    tibble::rownames_to_column(var="sample")
+  ref_sbs = intersect(colnames(expos_file), names(conversion_table))
+  ref_expos_i = referece_expos %>% dplyr::filter(cohort==cohortname, organ==organname) %>%
+    dplyr::select(sample, unlist(conversion_table[ref_sbs]) %>% setNames(NULL))
+
+  conv_expos = expos_file %>% dplyr::select(-dplyr::all_of(ref_sbs)) %>%
+    dplyr::full_join(ref_expos_i, by="sample") %>% tibble::column_to_rownames(var="sample") %>%
+    dplyr::mutate(organ=organname, cohort=cohortname)
+
+  assertthat::assert_that(sum(is.na(conv_expos))==0)
+
+  return(conv_expos)
+}) %>% dplyr::bind_rows() %>% replace(is.na(.), 0)
+
+saveRDS(expos_all, file=paste0(data_path, "expos_all.Rds"))
 
 
 
