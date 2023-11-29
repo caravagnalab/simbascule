5357dbec82d401951acf8de53c119c7e858d27c1
Elena Buscaroli <ele.buscaroli@gmail.com>
2023-11-29 17:12:15 +0100

    Update test_basilica.R
    adding test for clustering

--- nobuild/test_basilica.R
+++ nobuild/test_basilica.R
@@ -4,27 +4,58 @@ devtools::load_all("~/GitHub/simbasilica/")
 
 ## Simulated ####
 input.simul = readRDS("~/Dropbox/shared/2022. Basilica/datasets/input.N150_G2_simul.Rds")
-input.simul = readRDS("~/Dropbox/shared/2022. Basilica/simulations/fits/fits_dn.matched.2011/simul_fit.N150.G3.s9.matched.2011.Rds")$dataset
-input.simul %>% plot_fit()
+input.simul = readRDS("~/Dropbox/shared/2022. Basilica/simulations/fits/fits_dn.matched.2011/simul_fit.N150.G3.s9.matched.2011.Rds")
+x.simul = input.simul$fit.0
+x.simul$clustering = NULL
 
-counts = get_input(input.simul, matrix=T)
-# reference_cat = list("SBS"=COSMIC_filt[c("SBS1","SBS5"),])
-reference_cat = list("SBS"=COSMIC_filt[c("SBS1","SBS5"),],
-                     "DBS"=COSMIC_dbs[c("DBS2","DBS5"),])
-x.simul = fit(counts=counts,
-              k_list=3, # n of denovo signatures
-              cluster=6,  # n of clusters
-              reference_cat=reference_cat,
-              n_steps=1500, lr=0.005,
-              hyperparameters=list("scale_factor_centroid"=5000),
-              seed_list=c(19,33,2),
-              py=py)
+# counts = get_input(input.simul, matrix=T)
+# # reference_cat = list("SBS"=COSMIC_filt[c("SBS1","SBS5"),])
+# reference_cat = list("SBS"=COSMIC_filt[c("SBS1","SBS5"),],
+#                      "DBS"=COSMIC_dbs[c("DBS2","DBS5"),])
+# x.simul = fit(counts=counts,
+#               k_list=3, # n of denovo signatures
+#               # cluster=6,  # n of clusters
+#               reference_cat=reference_cat,
+#               n_steps=3000, lr=0.005,
+#               # hyperparameters=list("scale_factor_centroid"=10000, "scale_factor_alpha"=10000),
+#               seed_list=c(19,33,2),
+#               py=py)
 
-x.simul %>% get_initial_object() %>% plot_exposures()
-x.simul %>% get_initial_object() %>% plot_centroids()
+pi_list = c(0.6); n_clusters = 6; fits = c()
+fits = c(fits, lapply(pi_list, function(pi_conc0) {
+  x.cl = fit_clustering(x.simul, cluster=n_clusters,
+                        n_steps=3000, lr=0.005,
+                        # scale_factor_centroid = 1 and scale_factor_alpha = 1000 -> good centroids, bad clustering
+                        # scale_factor_centroid = 1 and scale_factor_alpha = 1 -> good centroids, bad clustering
+                        hyperparameters=list(
+                          "pi_conc0"=pi_conc0,
+                          "scale_factor_alpha"=1,
+                          "scale_factor_centroid"=1
+                          ),
+                        seed_list=c(33),
+                        py=py)
+  return(x.cl)
+}) %>% setNames(paste0("pi_",pi_list,"_G_", n_clusters))
+)
 
-x.simul %>% plot_exposures()
-x.simul %>% plot_centroids()
+x.cl = fits$pi_0.6_G_6
+
+x.cl %>% get_initial_object() %>% plot_mixture_weights() %>%
+  patchwork::wrap_plots(x.cl %>% plot_mixture_weights(), ncol=1, guides="collect")
+x.cl %>% get_initial_object() %>% plot_exposures() %>%
+  patchwork::wrap_plots(x.cl %>% plot_exposures(), ncol=1, guides="collect")
+x.cl %>% get_initial_object() %>% plot_centroids() %>%
+  patchwork::wrap_plots(x.cl %>% plot_centroids(), ncol=2, guides="collect")
+x.cl %>% plot_gradient_norms()
+x.cl %>% plot_posterior_probs()
+
+get_params(x.cl, what="clustering")
+
+# lapply(fits, function(fitid) {
+#   fitid %>% get_initial_object() %>% plot_mixture_weights() %>%
+#     patchwork::wrap_plots(fitid %>% plot_mixture_weights(), ncol=1, guides="collect") &
+#     patchwork::plot_annotation(title=names(fitid))
+# }) %>% patchwork::wrap_plots()
 
 plot_fit(x.simul)
 plot_QC(x.simul)
