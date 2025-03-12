8b4584b2b540642b3e37967e9224705bffdd7b30
elena-buscaroli <ele.buscaroli@gmail.com>
2025-03-12 13:11:54 +0100

    deleting old files

--- nobuild/sbatch/sbatch_old/sbatch_2507/run_sbatch.sh
+++
@@ -1,5 +0,0 @@
-sbatch 1.nonparam.sbatch
-sbatch 2.nonparam.sbatch
-
-sbatch 1.param.sbatch
-sbatch 2.param.sbatch
--- nobuild/sbatch/sbatch_old/sbatch_2706/sbatch_modA.sh
+++
@@ -1,7 +0,0 @@
-sbatch 1.modA_cosine.sbatch
-sbatch 1.modA_noreg.sbatch
-
-sbatch 2.modA_cosine.sbatch
-sbatch 2.modA_noreg.sbatch
-
-
